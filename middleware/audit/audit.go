6a3b6eb1fbe9c0ab643a5a87f4702ffc4d8bf307
Alex Barlow <a@alexb.cc>
2019-02-08 10:11:26 +0000

    Get audit user from context and metadata

--- middleware/audit/audit.go
+++ middleware/audit/audit.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/lileio/pubsub"
+	"google.golang.org/grpc/metadata"
 )
 
 // Middleware provides passing on the audit user for pubsub
@@ -13,6 +14,7 @@ type Middleware struct{}
 func (o Middleware) SubscribeInterceptor(opts pubsub.HandlerOptions, next pubsub.MsgHandler) pubsub.MsgHandler {
 	return func(ctx context.Context, m pubsub.Msg) error {
 		ctx = context.WithValue(ctx, "x-audit-user", m.Metadata["x-audit-user"])
+		ctx = metadata.NewIncomingContext(ctx, metadata.New(m.Metadata))
 		return next(ctx, m)
 	}
 }
@@ -20,11 +22,20 @@ func (o Middleware) SubscribeInterceptor(opts pubsub.HandlerOptions, next pubsub
 // PublisherMsgInterceptor inserts audit user into pubsub metadata
 func (o Middleware) PublisherMsgInterceptor(serviceName string, next pubsub.PublishHandler) pubsub.PublishHandler {
 	return func(ctx context.Context, topic string, m *pubsub.Msg) error {
-		user, ok := ctx.Value("x-audit-user").(string)
-		if ok {
-			m.Metadata["x-audit-user"] = user
+		var user string
+		audit, ok := ctx.Value("x-audit-user").(string)
+		if ok && audit != "" {
+			user = audit
 		}
 
+		md, ok := metadata.FromIncomingContext(ctx)
+		if ok {
+			if len(md["x-audit-user"]) > 0 {
+				user = md["x-audit-user"][0]
+			}
+		}
+
+		m.Metadata["x-audit-user"] = user
 		return next(ctx, topic, m)
 	}
 }
