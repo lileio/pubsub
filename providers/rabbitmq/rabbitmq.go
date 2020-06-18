7ff5836e1ee1d07f8c03006fd3b8bcbe7d99cb76
nadilas <janosveres@me.com>
2020-06-18 16:27:50 +0200

    Add publish and refactor fixes issues

--- providers/rabbitmq/rabbitmq.go
+++ providers/rabbitmq/rabbitmq.go
@@ -2,48 +2,125 @@ package rabbitmq
 
 import (
 	"context"
-	"crypto/sha1"
 	"fmt"
-	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
-
+	
 	"github.com/lileio/pubsub/v2"
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
-// session composes an amqp.Connection with an amqp.Channel
-type session struct {
-	*amqp.Connection
-	*amqp.Channel
+const RoutingKey = "RoutingKey"
+
+type PublishExchange struct{}
+
+var (
+	defaultConfig = ProviderConfig{
+		AppId:                 "",
+		EagerPublishExchanges: nil,
+		MaxRedialCount:        5,
+		RoutingKeySeparator:   "@",
+	}
+)
+
+type ProviderConfig struct {
+	AmqpUrl               string
+	AppId                 string
+	EagerPublishExchanges []string
+	MaxRedialCount        int
+	RoutingKeySeparator   string
 }
 
-type RabbitMQProvider struct {
-	amqpUrl    string
-	conn       *amqp.Connection
-	ch         *amqp.Channel
-	pubsubDone context.CancelFunc
-	shutdown   bool
+type Provider struct {
+	config ProviderConfig
+	conn   *amqp.Connection
+	ch     *amqp.Channel
+
+	// To synchronize shutdown
+	quit *Event
+
+	// exchange channel map
+	exchangeQueuesMux sync.RWMutex
+	exchangeQueues    map[string]chan *pubsub.Msg
+}
+
+func mergeConfig(config ProviderConfig) ProviderConfig {
+	cfg := defaultConfig
+	if config.AppId != "" {
+		cfg.AppId = config.AppId
+	}
+	if config.AmqpUrl != "" {
+		cfg.AmqpUrl = config.AmqpUrl
+	}
+	if config.MaxRedialCount != cfg.MaxRedialCount && config.MaxRedialCount != 0 {
+		cfg.MaxRedialCount = config.MaxRedialCount
+	}
+	return cfg
 }
 
 // Creates a new Subscriber and sets the AMQP url for the internal rabbitMQ client
-func NewRabbitMQProvider(amqpUrl string) *RabbitMQProvider {
-	return &RabbitMQProvider{amqpUrl: amqpUrl}
+//
+// You can send a
+func NewProvider(config ProviderConfig, eagerExchanges ...string) *Provider {
+	providerCfg := mergeConfig(config)
+	providerCfg.EagerPublishExchanges = eagerExchanges
+
+	p := &Provider{
+		config:            providerCfg,
+		exchangeQueuesMux: sync.RWMutex{},
+		exchangeQueues:    make(map[string]chan *pubsub.Msg),
+		quit:              NewEvent(),
+	}
+
+	// setup eager exchanges if any
+	go p.setupPublishTopics()
+
+	return p
 }
 
-func (r *RabbitMQProvider) Publish(ctx context.Context, topic string, m *pubsub.Msg) error {
-	panic("implement me")
+// Publish queues a message to a topic (a.k.a an exchange)
+//
+// In order to supply a routing key for a topic use the following syntax when supplying the topic parameter:
+//  // the default config.RoutingKeySeparator is '@'
+//  topic := "routingKeys@TopicExchange"
+//
+// You can also use the helper method provider.RoutingKeyAtExchangeHelper, which uses the separator from the config
+func (p *Provider) Publish(ctx context.Context, topic string, message *pubsub.Msg) error {
+	sp, _ := opentracing.StartSpanFromContext(ctx, currentFunc())
+	defer sp.Finish()
+
+	exchange, routing, err := p.extractExchangeAndRouting(topic)
+	if err != nil {
+		return err
+	}
+	queue, err := p.exchangeQueue(exchange)
+	if err != nil {
+		return err
+	}
+	message.Metadata[RoutingKey] = routing
+
+	// post message
+	queue <- message
+	return nil
 }
 
-func (r *RabbitMQProvider) Subscribe(opts pubsub.HandlerOptions, handler pubsub.MsgHandler) {
+// Subscribes to a specific exchange by creating a new queue consumer
+//
+// By default the routing key is derived from the Name field of the opts parameter.
+// In order to set an empty routing key, use "-" for the mandatory pubsub.HandlerOptions.Name parameter
+func (p *Provider) Subscribe(opts pubsub.HandlerOptions, handler pubsub.MsgHandler) {
 	go func() {
 		queue := identity()
 
-		for session := range r.redial(context.Background(), r.amqpUrl) {
+		for session := range p.redial() {
 			sub := <-session
 
-			if _, err := sub.QueueDeclare(queue, false, true, true, false, nil); err != nil {
+			if _, err := sub.QueueDeclare(queue, false, true, false, false, nil); err != nil {
 				logrus.Warnf("cannot consume from exclusive queue: %q, %v", queue, err)
 				return
 			}
@@ -59,16 +136,17 @@ func (r *RabbitMQProvider) Subscribe(opts pubsub.HandlerOptions, handler pubsub.
 				return
 			}
 
-			deliveries, err := sub.Consume(queue, "", opts.AutoAck, true, false, false, nil)
+			deliveries, err := sub.Consume(queue, "", opts.AutoAck, false, false, false, nil)
 			if err != nil {
 				logrus.Warnf("cannot consume from: %q, %v", queue, err)
 				return
 			}
 
-			logrus.Infof("Queue (%s) attached to exchange '%s' via routing key '%s'", queue, exchange, routingKey)
+			logrus.Infof("Queue (%s) attached to exchange '%s/%s' via routing key '%s'", queue, sub.Connection.Config.Vhost, exchange, routingKey)
 
 			for msg := range deliveries {
-				if r.shutdown {
+				if p.quit.HasFired() {
+					logrus.Infof("Shutting down queue (%s) attached to exchange '%s/%s' via routing key '%s'", queue, sub.Connection.Config.Vhost, exchange, routingKey)
 					break
 				}
 
@@ -121,32 +199,233 @@ func (r *RabbitMQProvider) Subscribe(opts pubsub.HandlerOptions, handler pubsub.
 	}()
 }
 
-// redial continually connects to the URL, exiting the program when no longer possible
-func (r *RabbitMQProvider) redial(ctx context.Context, url string) chan chan session {
+// Shutdown is to be called for graceful termination
+func (p *Provider) Shutdown() {
+	// trigger shutdown
+	p.quit.Fire()
+
+	// TODO should wait for all go-rotuines to shut down
+
+	if p.ch != nil {
+		_ = p.ch.Close()
+	}
+	if p.conn != nil {
+		_ = p.conn.Close()
+	}
+}
+
+// Helpers
+
+// RoutingKeyAtExchangeHelper is a shorthand for creating a publish topic string
+//
+// It is essentially the same as calling:
+//  topic := fmt.Sprintf("%s%s%s", routingKey, p.config.RoutingKeySeparator, exchange)
+func (p *Provider) RoutingKeyAtExchangeHelper(routingKey, exchange string) (topic string) {
+	return fmt.Sprintf("%s%s%s", routingKey, p.config.RoutingKeySeparator, exchange)
+}
+
+// Prepares and creates queues for eager publish exchanges and starts processing on them
+func (p *Provider) setupPublishTopics() {
+	for _, exchange := range p.config.EagerPublishExchanges {
+		exchange, _, err := p.extractExchangeAndRouting(exchange)
+		if err != nil {
+			logrus.Warnf("Failed to eagerly prepare publish exchange: %s. %v", exchange, err)
+			continue
+		}
+		_, err = p.exchangeQueue(exchange)
+		if err != nil {
+			logrus.Warnf("Failed to eagerly create queue for publish exchange: %s. %v", exchange, err)
+		}
+	}
+}
+
+// Returns the underlying queue for this topic exchange
+//
+// If the exchange has not been used before, start a new redial go-routine for processing the queue
+func (p *Provider) exchangeQueue(exchange string) (chan *pubsub.Msg, error) {
+	p.exchangeQueuesMux.RLock()
+	if xch, ok := p.exchangeQueues[exchange]; !ok {
+		p.exchangeQueuesMux.RUnlock()
+		// create new queue
+		newQueue := make(chan *pubsub.Msg, 10)
+
+		// save for reuse
+		p.exchangeQueuesMux.Lock()
+		p.exchangeQueues[exchange] = newQueue
+		p.exchangeQueuesMux.Unlock()
+
+		// start processing until context exists
+		go p.publishToExchange(p.redial(), exchange, newQueue)
+
+		return newQueue, nil
+	} else {
+		p.exchangeQueuesMux.RUnlock()
+		return xch, nil
+	}
+}
+
+// Separates the incoming publish topic to exchange name and routing key
+func (p *Provider) extractExchangeAndRouting(ogTopic string) (exchange, routingKey string, err error) {
+	if !strings.Contains(ogTopic, p.config.RoutingKeySeparator) {
+		return ogTopic, "", nil // is exchange
+	}
+	s := strings.Split(ogTopic, p.config.RoutingKeySeparator)
+	if len(s) < 2 {
+		return "", "", errors.New(fmt.Sprintf("Too many separators '%s'. Syntax is: '$routingKey%s$exchange'", p.config.RoutingKeySeparator, p.config.RoutingKeySeparator))
+	}
+	return s[1], s[0], nil
+}
+
+// publishToTopic keeps a reconnecting session to a topic exchange open and waits for queued messages.
+//
+// It receives from the application specific source of messages.
+func (p *Provider) publishToExchange(sessions chan chan session, exchange string, messages <-chan *pubsub.Msg) {
+	for session := range sessions {
+		var (
+			running bool
+			reading = messages
+			pending = make(chan *pubsub.Msg, 1)
+			confirm = make(chan amqp.Confirmation, 1)
+		)
+
+		pub := <-session
+
+		// publisher confirms for this channel/connection
+		if err := pub.Confirm(false); err != nil {
+			logrus.Warnf("publisher confirms not supported")
+			close(confirm) // confirms not supported, simulate by always nacking
+		} else {
+			pub.NotifyPublish(confirm)
+		}
+
+	Publish:
+		for {
+			if p.quit.HasFired() {
+				logrus.Infof("Shutting down publishToExchange: %s/%s...", p.conn.Config.Vhost, exchange)
+				if pending != nil {
+					close(pending)
+				}
+				if confirm != nil {
+					close(confirm)
+				}
+				return
+			}
+			var msg *pubsub.Msg
+			select {
+			// confirm
+			case confirmed, ok := <-confirm:
+				if !ok {
+					break Publish
+				}
+				if !confirmed.Ack {
+					logrus.Debugf("nack message %d, msg: %q", confirmed.DeliveryTag, string(msg.Data))
+				}
+				reading = messages
+			// pending
+			case msg = <-pending:
+				routingKey := "" // "ignored for fanout exchanges, application dependent for other exchanges"
+				if rk, ok := msg.Metadata[RoutingKey]; ok {
+					routingKey = rk
+				}
+
+				// transfer metadata
+				headers := amqp.Table{}
+				for k, v := range msg.Metadata {
+					headers[k] = v
+				}
+				publishing := amqp.Publishing{
+					Headers: headers,
+					// ContentType:     "",
+					// ContentEncoding: "",
+					// DeliveryMode:    0,
+					// Priority:        0,
+					// CorrelationId:   "",
+					// ReplyTo:         "",
+					// Expiration:      "",
+					// Type:            "",
+					// UserId:          "",
+					AppId: p.config.AppId,
+					Body:  msg.Data,
+				}
+				if msg.ID != "" {
+					publishing.MessageId = msg.ID
+				}
+				if msg.PublishTime != nil {
+					publishing.Timestamp = *msg.PublishTime
+				}
+				err := pub.Publish(exchange, routingKey, false, false, publishing)
+				// Retry failed delivery on the next session
+				if err != nil {
+					logrus.Errorf("failed to publish %q. %v", string(msg.Data), err)
+					pending <- msg
+					_ = pub.Channel.Close()
+					break Publish
+				} else {
+					// write back publish time
+					if msg.PublishTime == nil {
+						now := time.Now()
+						msg.PublishTime = &now
+					}
+					logrus.Debugf("sent %q", string(msg.Data))
+				}
+			// reading
+			case msg, running = <-reading:
+				// all messages consumed
+				if !running {
+					return
+				}
+				// work on pending delivery until ack'd
+				pending <- msg
+				reading = nil
+			// loop over for shudown check
+			case <-time.After(time.Millisecond * 100):
+			}
+		}
+	}
+}
+
+// redial continually connects to the URL
+//
+// This method exits the program when retry count reaches max retry count defined in the config
+func (p *Provider) redial() chan chan session {
 	sessions := make(chan chan session)
 
 	go func() {
 		sess := make(chan session)
-		defer close(sessions)
-
+		defer func() {
+			close(sessions)
+			close(sess)
+		}()
+		retryCounter := 1
 		for {
 			select {
 			case sessions <- sess:
-			case <-ctx.Done():
-				logrus.Debugf("shutting down session factory")
+			case <-p.quit.Done():
+				logrus.Info("Shutting down redial:sessionfactory...")
 				return
 			}
 
-			if r.conn == nil {
+			if p.conn == nil {
 				// initialize connection only once
 				var err error
-				r.conn, err = amqp.Dial(url)
+				p.conn, err = amqp.Dial(p.config.AmqpUrl)
 				if err != nil {
-					logrus.Fatalf("cannot (re)dial: %v: %q", err, url)
+					if retryCounter > p.config.MaxRedialCount {
+						logrus.Fatalf("cannot (re)dial: %v: %q", err, p.config.AmqpUrl)
+					} else {
+						sleep := time.Second * time.Duration(retryCounter)
+						logrus.Errorf("waiting %s before redialing AQMP address. Error: %v")
+						// sleep before next retry
+						time.Sleep(sleep)
+						retryCounter++
+						continue // try again
+					}
+				} else {
+					retryCounter = 1 // reset retry counter
 				}
 			}
 
-			ch, err := r.conn.Channel()
+			ch, err := p.conn.Channel()
 			if err != nil {
 				logrus.Fatalf("cannot create channel: %v", err)
 			}
@@ -157,43 +436,13 @@ func (r *RabbitMQProvider) redial(ctx context.Context, url string) chan chan ses
 			//}
 
 			select {
-			case sess <- session{r.conn, ch}:
-			case <-ctx.Done():
-				logrus.Debugf("shutting down new session")
+			case sess <- session{p.conn, ch}:
+			case <-p.quit.Done():
+				logrus.Info("Shutting down redial:sessions...")
 				return
 			}
 		}
 	}()
 
 	return sessions
-}
-
-func (r *RabbitMQProvider) Shutdown() {
-	r.shutdown = true
-	if r.pubsubDone != nil {
-		r.pubsubDone()
-	}
-	if r.ch != nil {
-		_ = r.ch.Close()
-	}
-	if r.conn != nil {
-		_ = r.conn.Close()
-	}
-}
-
-// identity returns the same host/process unique string for the lifetime of
-// this process so that subscriber reconnections reuse the same queue name.
-func identity() string {
-	hostname, err := os.Hostname()
-	h := sha1.New()
-	_, _ = fmt.Fprint(h, hostname)
-	_, _ = fmt.Fprint(h, err)
-	_, _ = fmt.Fprint(h, os.Getpid())
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-func warnOnError(err error, msg string) {
-	if err != nil {
-		logrus.Warnf("%s: %s", msg, err)
-	}
 }
