050e0d7144a15b690a26bca665530bf7d0d325c8
Alex Barlow <a@alexb.cc>
2021-09-29 11:09:58 +0100

    Add Cloud Run Pub Sub Push support

--- go.mod
+++ go.mod
@@ -1,34 +1,34 @@
 module github.com/lileio/pubsub/v2
 
 require (
-	cloud.google.com/go v0.34.0
+	cloud.google.com/go v0.95.0
+	cloud.google.com/go/pubsub v1.17.0
+	github.com/apache/thrift v0.0.0-20181016064013-5c1ecb67cde4 // indirect
 	github.com/dropbox/godropbox v0.0.0-20180512210157-31879d3884b9
+	github.com/go-logfmt/logfmt v0.3.0 // indirect
 	github.com/gofrs/uuid v3.1.0+incompatible
-	github.com/golang/protobuf v1.3.2
-	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/golang/protobuf v1.5.2
 	github.com/jpillora/backoff v0.0.0-20180909062703-3050d21c67d7
 	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515 // indirect
-	github.com/lileio/fromenv v0.0.0-20180720125005-e881ea03b503
-	github.com/lileio/lile v0.0.0-20181016094446-48bc7aaffacf
 	github.com/lileio/logr v1.1.0
 	github.com/nats-io/nats-server/v2 v2.1.0 // indirect
 	github.com/nats-io/nats-streaming-server v0.16.2 // indirect
 	github.com/nats-io/stan.go v0.5.0
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/openzipkin/zipkin-go-opentracing v0.3.4
+	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v0.9.2
 	github.com/segmentio/kafka-go v0.1.0
 	github.com/segmentio/ksuid v1.0.2
 	github.com/sirupsen/logrus v1.4.2
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.6.1
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1
-	golang.org/x/oauth2 v0.0.0-20181003184128-c57b0facaced // indirect
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
-	google.golang.org/genproto v0.0.0-20181004005441-af9cb2a35e7f // indirect
-	google.golang.org/grpc v1.24.0
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	google.golang.org/grpc v1.40.0
 )
 
 go 1.13
---
+++ providers/googlecloudrun/googlecloudrun.go
@@ -0,0 +1,293 @@
+package googlecloudrun
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"cloud.google.com/go/compute/metadata"
+	"cloud.google.com/go/pubsub"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/lileio/logr"
+	ps "github.com/lileio/pubsub/v2"
+	"golang.org/x/oauth2/google"
+
+	"github.com/sirupsen/logrus"
+)
+
+const httpPath = "/pubsub"
+const queryName = "subName"
+
+var (
+	mutex = &sync.Mutex{}
+)
+
+// GoogleCloudRun provides google cloud pubsub but specifically tailored to Google Cloud Run, in that is works over http and push and not a long running process
+type GoogleCloudRun struct {
+	client   *pubsub.Client
+	topics   map[string]*pubsub.Topic
+	subs     map[string]ps.MsgHandler
+	shutdown bool
+}
+
+type PushRequest struct {
+	Message struct {
+		Attributes  map[string]string `json:"attributes"`
+		Data        []byte            `json:"data"`
+		ID          string            `json:"message_id"`
+		PublishTime time.Time         `json:"publish_time"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
+}
+
+// NewGoogleCloudRun creates a new GoogleCloudRun instace for a project
+func NewGoogleCloudRun(projectID string) (*GoogleCloudRun, error) {
+	c, err := pubsub.NewClient(context.Background(), projectID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GoogleCloudRun{
+		client: c,
+		topics: map[string]*pubsub.Topic{},
+		subs:   map[string]ps.MsgHandler{},
+	}, nil
+}
+
+func (g *GoogleCloudRun) RegisterHandler(m http.ServeMux) {
+	m.HandleFunc(httpPath, func(w http.ResponseWriter, r *http.Request) {
+		var m PushRequest
+		if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+			http.Error(w, fmt.Sprintf("Could not decode body: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		sub := r.URL.Query().Get(queryName)
+		h, ok := g.subs[sub]
+		if !ok {
+			http.Error(w, fmt.Sprintf("No subscription handler found"), http.StatusInternalServerError)
+			return
+		}
+
+		msg := ps.Msg{
+			ID:          m.Message.ID,
+			Metadata:    m.Message.Attributes,
+			Data:        m.Message.Data,
+			PublishTime: &m.Message.PublishTime,
+			Ack: func() {
+				// Only auto ack is supported for cloud run
+			},
+			Nack: func() {
+				http.Error(w, fmt.Sprintf("Cannot process this message, NACK"), http.StatusInternalServerError)
+				return
+			},
+		}
+
+		ctx := context.Background()
+		err := h(ctx, msg)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error processing message: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		// All good we can 200
+		w.WriteHeader(http.StatusOK)
+		return
+	})
+}
+
+// Publish implements Publish
+func (g *GoogleCloudRun) Publish(ctx context.Context, topic string, m *ps.Msg) error {
+	t, err := g.getTopic(topic)
+	if err != nil {
+		return err
+	}
+
+	logr.WithCtx(ctx).Debug("Google Pubsub: Publishing")
+	res := t.Publish(context.Background(), &pubsub.Message{
+		Data:       m.Data,
+		Attributes: m.Metadata,
+	})
+
+	_, err = res.Get(context.Background())
+	if err != nil {
+		logr.WithCtx(ctx).Error(errors.Wrap(err, "publish get failed"))
+	} else {
+		logr.WithCtx(ctx).Debug("Google Pubsub: Publish confirmed")
+	}
+
+	return err
+}
+
+// Subscribe implements Subscribe
+func (g *GoogleCloudRun) Subscribe(opts ps.HandlerOptions, h ps.MsgHandler) {
+	g.subscribe(opts, h, make(chan bool, 1))
+}
+
+// Shutdown shuts down all subscribers gracefully
+func (g *GoogleCloudRun) Shutdown() {
+	// We rely on the http server to do a graceful shutdown
+	g.shutdown = true
+	return
+}
+
+type CloudRunAPIUrlOnly struct {
+	Status struct {
+		URL string `json:"url"`
+	} `json:"status"`
+}
+
+func getCloudRunUrl() (string, error) {
+	projectNumber, region, err := getProjectAndRegion()
+	if err != nil {
+		return "", fmt.Errorf("impossible to get the projectNumber and region from the metadata server with error %+v\n", err)
+	}
+
+	// Get the service name from the environment variables
+	service := os.Getenv("K_SERVICE")
+	if service == "" {
+		return "", fmt.Errorf("impossible to get the Cloud Run service name from Environment Variable with error %+v\n", err)
+	}
+
+	ctx := context.Background()
+	// To perform a call the Cloud Run API, the current service, through the service account, needs to be authenticated
+	// The Google Auth default client add automatically the authorization header for that.
+	client, err := google.DefaultClient(ctx)
+
+	// Build the request to the API
+	cloudRunApi := fmt.Sprintf(
+		"https://%s-run.googleapis.com/apis/serving.knative.dev/v1/namespaces/%s/services/%s",
+		region,
+		projectNumber,
+		service,
+	)
+	// Perform the call
+	resp, err := client.Get(cloudRunApi)
+	if err != nil {
+		return "", fmt.Errorf("error when calling the Cloud Run API %s with error %+v\n", cloudRunApi, err)
+	}
+	defer resp.Body.Close()
+
+	// Read the body of the response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("impossible to read the Cloud Run API call body with error %+v\n", err)
+	}
+
+	// Map the JSON body in a minimal struct. We need only the URL, the struct match only this part in the JSON.
+	cloudRunResp := &CloudRunAPIUrlOnly{}
+	json.Unmarshal(body, cloudRunResp)
+	return cloudRunResp.Status.URL, nil
+}
+
+func getProjectAndRegion() (projectNumber string, region string, err error) {
+	// Get the region from the metadata server. The project number is returned in the response
+	resp, err := metadata.Get("/instance/region")
+	if err != nil {
+		return
+	}
+	// response pattern is projects/<projectNumber>/regions/<region>
+	r := strings.Split(resp, "/")
+	projectNumber = r[1]
+	region = r[3]
+	return
+}
+
+func (g *GoogleCloudRun) subscribe(opts ps.HandlerOptions, h ps.MsgHandler, ready chan<- bool) {
+	var err error
+	subName := opts.ServiceName + "-" + opts.Name + "--" + opts.Topic
+	sub := g.client.Subscription(subName)
+
+	t, err := g.getTopic(opts.Topic)
+	if err != nil {
+		logrus.Panicf("Can't fetch topic: %s", err.Error())
+	}
+
+	ok, err := sub.Exists(context.Background())
+	if err != nil {
+		logrus.Panicf("Can't connect to pubsub: %s", err.Error())
+	}
+
+	if !ok {
+		serviceURL, err := getCloudRunUrl()
+		if err != nil {
+			logrus.Panicf("Can't subscribe pubsub topic %s, because: %s", subName, err.Error())
+		}
+
+		u, err := url.Parse(serviceURL)
+		if err != nil {
+			logrus.Panicf("Can't subscribe pubsub topic %s, because service url isn't valid: %s", subName, err.Error())
+		}
+
+		if os.Getenv("PUBSUB_SERVICE_ACCOUNT_EMAIL") == "" {
+			logrus.Panicf("Can't subscribe pubsub topic %s, PUBSUB_SERVICE_ACCOUNT_EMAIL must be set for invoke cloud run functions", subName)
+		}
+
+		// Create a url endpoint like "https://hello-world-askdf6b6k.a.run.app/pubsub?subName=someuniqusubscription"
+		u.Path = httpPath
+		q := u.Query()
+		q.Set(queryName, subName)
+		u.RawQuery = q.Encode()
+
+		sc := pubsub.SubscriptionConfig{
+			Topic:       t,
+			AckDeadline: opts.Deadline,
+			PushConfig: pubsub.PushConfig{
+				Endpoint: u.String(),
+				AuthenticationMethod: &pubsub.OIDCToken{
+					ServiceAccountEmail: os.Getenv("PUBSUB_SERVICE_ACCOUNT_EMAIL"),
+				},
+			},
+		}
+
+		sub, err = g.client.CreateSubscription(context.Background(), subName, sc)
+		if err != nil {
+			logrus.Panicf("Can't subscribe to topic: %s", err.Error())
+		}
+	}
+
+	g.subs[subName] = h
+}
+
+func (g *GoogleCloudRun) getTopic(name string) (*pubsub.Topic, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if g.topics[name] != nil {
+		return g.topics[name], nil
+	}
+
+	var err error
+	t := g.client.Topic(name)
+	ok, err := t.Exists(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		t, err = g.client.CreateTopic(context.Background(), name)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	g.topics[name] = t
+
+	return t, nil
+}
+
+func (g *GoogleCloudRun) deleteTopic(name string) error {
+	t, err := g.getTopic(name)
+	if err != nil {
+		return err
+	}
+
+	return t.Delete(context.Background())
+}
